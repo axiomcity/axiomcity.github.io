7cb5f4c6442d480d5199d4b66e25551c36be4d08
davidos_studi <david.ber0169@gmail.com>
2025-07-14 23:41:08 +0200

    a

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function myFunction() {
+function burger1() {
     var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+        x.style.flexDirection = "column";
+        x.style.backgroundColor = "#001520"
+    }
+}
+
+function burger2() {
+    var x = document.getElementById("topics");
     if (x.style.display === "flex") {
         x.style.display = "none";
     } else {
