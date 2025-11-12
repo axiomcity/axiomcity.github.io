031c71aa57848fdb2334af193ed03558a55e37a3
Your Name <you@example.com>
2025-11-12 11:06:03 +0100

    Langues

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,32 @@ const burger2 = document.getElementById("burger2");
 const accessibility = document.getElementById('accessibility');
 const site = document.getElementById('site');
 
+
+// #############################
+// LANGUES
+const lang_info = document.getElementById("lang-info");
+const lang_select = document.getElementById("lang-select");
+const vid_content = document.getElementById("vid-content");
+
+function vid_lang() {
+    let language = lang_select.options[lang_select.selectedIndex];
+    lang_info.innerHTML = language.innerHTML;
+
+    // Charger la video sur la langue choisi dans le parametre du bouton select
+    switch(language.value) {
+        case "en":
+            vid_content.src = "assets/img/videos/demo-en.mp4";break
+        case "ru":
+            vid_content.src = "assets/img/videos/demo-ru.mp4";break
+        case "de":
+            vid_content.src = "assets/img/videos/demo-de.mp4";break
+        case "fr":
+            vid_content.src = "assets/img/videos/demo-fr.mp4";break
+    }
+    console.log("select : ", language.value, language.innerHTML, vid_content.src);
+}
+
+
 // Define localStorages Cookies
 site.style.filter = localStorage.getItem('style');
 
